7f566334d83e71fe1d497df1d6a2a19be82d91d9
Naloween <40539093+Naloween@users.noreply.github.com>
2025-11-14 23:24:46 +0100

    rename web main file to index.html

--- fighting_forms_client/Menus/GameSelect/game_button.gd
+++ fighting_forms_client/Menus/GameSelect/game_button.gd
@@ -2,6 +2,7 @@ extends Button
 class_name GameButton
 
 var game_id: int
+var creator_name: String
 
 signal enter_game(int)
 
@@ -12,10 +13,19 @@ static func create_game_button(game_id: int) -> GameButton:
 	return node
 
 func _ready() -> void:
+	SpacetimeDB.FightingForms.db.game.on_update(_on_game_update)
+	
 	var game = SpacetimeDB.FightingForms.db.game.id.find(game_id)
 	var creator = SpacetimeDB.FightingForms.db.player.id.find(game.creator_id)
+	creator_name = creator.name
 	
-	text = creator.name+"'s Game\n 1/4"
+	text = creator_name+"'s Game\n " + str(len(game.players)) + "/" + str(game.max_nb_players)
 
 func _on_pressed() -> void:
 	enter_game.emit(game_id)
+
+func _on_game_update(old_game: FightingFormsGame, new_game: FightingFormsGame):
+	text = creator_name+"'s Game\n " + str(len(new_game.players)) + "/" + str(new_game.max_nb_players)
+
+func _exit_tree() -> void:
+	SpacetimeDB.FightingForms.db.game.remove_on_update(_on_game_update)
--- publish_website.bash
+++ publish_website.bash
@@ -1,2 +1,1 @@
-cp ./fighting_forms_client/builds/web/FightingForms.html ./fighting_forms_client/builds/web/index.html
 git subtree push --prefix fighting_forms_client/builds/web  origin gh-pages
